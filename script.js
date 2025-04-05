a6f5352a14edcbd8ba844a8d19e62b498fb22a5a
preet-pookiehost <dstudio335@gmail.com>
2025-04-05 17:38:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function stopBot(botId) {
+  alert("Sending stop command for: " + botId);
+
+  // In future, connect to backend API
+  // fetch("https://your-cloudflare-link/api/stop_bot", {
+  //   method: "POST",
+  //   body: JSON.stringify({ botId }),
+  // });
+}
